c43eabe1da94bcfd2104643e1610d1401d365605
Massimo Di Berardino <maxdo1996@gmail.com>
2020-02-23 22:26:28 -0300

    user content and styles

--- challenge/frontend/src/components/UserInfo.jsx
+++ challenge/frontend/src/components/UserInfo.jsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import React from "react";
+import '../assets/styles/components/UserInfo.scss';
 
-const UserInfo = () => (
-    <h1>User Info</h1>
-);
+const UserInfo = () => {
+  return (
+    <div className="info__container">
+      <img
+        className="info__container--img"
+        src=""
+        alt="user-image"
+      />
+      <h2>
+        Massimo Di Berardino
+      </h2>
+      <h3>example@email.com</h3>
+      <h4>23 Years old</h4>
+      <h4>Admin</h4>
+    </div>
+  );
+};
 
 export default UserInfo;
