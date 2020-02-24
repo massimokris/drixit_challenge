fc360c77a303665d536a557dcddf83de61151255
Massimo Di Berardino <maxdo1996@gmail.com>
2020-02-23 23:22:13 -0300

    adding an action to authenticate the user

--- challenge/frontend/src/actions/index.js
+++ challenge/frontend/src/actions/index.js
@@ -1,4 +1,22 @@
+import axios from "axios";
+
 export const loginRequest = payload => ({
   type: "LOGIN_REQUEST",
   payload
 });
+
+export const loginUser = ({ email, password }) => async dispatch => {
+  await axios({
+    url: "http://localhost:3000/api/v0/authenticate",
+    method: "POST",
+    data: {
+      email,
+      password
+    }
+  })
+    .then(async ({ data }) => {
+      document.cookie = `token=${data.token}; Max-Age=7400`;
+      dispatch(loginRequest(data));
+    })
+    .catch(err => console.log(err.message));
+};
--- challenge/frontend/src/components/Login.jsx
+++ challenge/frontend/src/components/Login.jsx
@@ -1,68 +1,74 @@
-import React, { useState } from 'react';
-import { loginRequest } from '../actions';
-import { connect } from 'react-redux';
-import '../assets/styles/components/Login.scss';
+import React, { useState } from "react";
+import { loginUser } from "../actions";
+import { connect } from "react-redux";
+import "../assets/styles/components/Login.scss";
 
 const Login = props => {
-    const [form, setValues] = useState({
-        email: ""
+  const [form, setValues] = useState({
+    email: ""
+  });
+
+  const handleInput = event => {
+    setValues({
+      ...form,
+      [event.target.name]: event.target.value
     });
+    if (event.target.name === "email") validEmail(event.target.value);
+  };
 
-    const handleInput = event => {
-        setValues({
-            ...form,
-            [event.target.name]: event.target.value
-        });
-        if(event.target.name === "email") validEmail(event.target.value);
+  const validEmail = email => {
+    const pattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if (email.match(pattern)) {
+      document.getElementsByClassName("button")[0].style.display = "block";
+      document.getElementsByClassName("input")[1].style.display = "block";
+    } else {
+      document.getElementsByClassName("button")[0].style.display = "none";
+      document.getElementsByClassName("input")[1].style.display = "none";
     }
+  };
 
-    const validEmail = email => {
-        const pattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;  
+  const handleSubmit = event => {
+    event.preventDefault();
+    props.loginUser(form);
+  };
 
-        if(email.match(pattern)){
-            document.getElementsByClassName('button')[0].style.display = 'block';
-            document.getElementsByClassName('input')[1].style.display = 'block';
-        } else {
-            document.getElementsByClassName('button')[0].style.display = 'none';
-            document.getElementsByClassName('input')[1].style.display = 'none';
-        }
-    }
-
-    const handleSubmit = event => {
-        event.preventDefault();
-        props.loginRequest(form);
-    }
-
-    return (
-        <div className="login__container">
-          <h2>Log in to your account</h2>
-          <form action="" className="login__container--form" onSubmit={handleSubmit}> 
-            <input
-              name="email"
-              aria-label="Email"
-              className="input"
-              type="text"
-              placeholder="Email Adress"
-              onChange={handleInput}
-              required
-            />
-            <input
-              name="password"
-              aria-label="Password"
-              className="input"
-              type="password"
-              placeholder="Password"
-              onChange={handleInput}
-              required
-            />
-            <button className="button" type="submit">Login</button>
-          </form>
-        </div>
-    );
-}
+  return (
+    <div className="login__container">
+      <h2>Log in to your account</h2>
+      <form
+        action=""
+        className="login__container--form"
+        onSubmit={handleSubmit}
+      >
+        <input
+          name="email"
+          aria-label="Email"
+          className="input"
+          type="text"
+          placeholder="Email Adress"
+          onChange={handleInput}
+          required
+        />
+        <input
+          name="password"
+          aria-label="Password"
+          className="input"
+          type="password"
+          placeholder="Password"
+          onChange={handleInput}
+          required
+        />
+        <button className="button" type="submit">
+          Login
+        </button>
+      </form>
+    </div>
+  );
+};
 
 const mapDispatchToProps = {
-    loginRequest
+  loginUser
 };
 
 export default connect(null, mapDispatchToProps)(Login);
