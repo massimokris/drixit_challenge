93cdfaa3f6ef3d6d3aae7cc1a9f156f2d34851ba
Massimo Di Berardino <maxdo1996@gmail.com>
2020-02-23 22:21:37 -0300

    login content and styles

--- challenge/frontend/src/components/Layout.jsx
+++ challenge/frontend/src/components/Layout.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React from "react";
+import "../assets/styles/components/Layout.scss";
 
-const Layout = ({ children}) => (
-    <section className="App">
-        {children}
-    </section>
+const Layout = ({ children }) => (
+  <section className="App">
+    {children}
+  </section>
 );
 
 export default Layout;
--- challenge/frontend/src/components/Login.jsx
+++ challenge/frontend/src/components/Login.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import '../assets/styles/components/Login.scss';
 
-const Login = () => (
-    <h1>Login</h1>
-);
+const Login = () => {
+    return (
+        <div className="login__container">
+          <h2>Log in to your account</h2>
+          <form action="" className="login__container--form"> 
+            <input
+              name="email"
+              aria-label="Email"
+              className="input"
+              type="text"
+              placeholder="Email Adress"
+              required
+            />
+            <input
+              name="password"
+              aria-label="Password"
+              className="input"
+              type="password"
+              placeholder="Password"
+              required
+            />
+            <button className="button" type="submit">Login</button>
+          </form>
+        </div>
+    );
+}
 
 export default Login;
