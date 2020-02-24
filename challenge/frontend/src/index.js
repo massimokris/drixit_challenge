d7412e832a49edc690eec53bba35fd8e2a3f1c34
Massimo Di Berardino <maxdo1996@gmail.com>
2020-02-23 22:51:54 -0300

    adding redux and the storage

--- challenge/frontend/src/index.js
+++ challenge/frontend/src/index.js
@@ -1,8 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import reducer from './reducers';
 import App from "./routes/App";
 
+const initialState = {
+  user: {}
+};
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+
+const store = createStore(
+  reducer,
+  initialState,
+  composeEnhancers(applyMiddleware(thunk))
+);
+
 ReactDOM.render(
-    <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById("app")
 );
