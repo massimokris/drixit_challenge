5b604e6410964631ac4bfd4e348ec0b1f2e0bc99
Massimo Di Berardino <maxdo1996@gmail.com>
2020-02-23 22:33:09 -0300

    getting form data

--- challenge/frontend/src/components/Login.jsx
+++ challenge/frontend/src/components/Login.jsx
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../assets/styles/components/Login.scss';
 
 const Login = () => {
+    const [form, setValues] = useState({
+        email: ""
+    });
+
+    const handleInput = event => {
+        setValues({
+            ...form,
+            [event.target.name]: event.target.value
+        });
+    }
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        console.log(form);
+    }
+
     return (
         <div className="login__container">
           <h2>Log in to your account</h2>
-          <form action="" className="login__container--form"> 
+          <form action="" className="login__container--form" onSubmit={handleSubmit}> 
             <input
               name="email"
               aria-label="Email"
               className="input"
               type="text"
               placeholder="Email Adress"
+              onChange={handleInput}
               required
             />
             <input
@@ -20,6 +37,7 @@ const Login = () => {
               className="input"
               type="password"
               placeholder="Password"
+              onChange={handleInput}
               required
             />
             <button className="button" type="submit">Login</button>
